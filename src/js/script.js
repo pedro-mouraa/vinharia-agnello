01c6faa62f35c8761d76db9f0743c9c0d58923db
Enzo <enzogabrielp86@gmail.com>
2026-03-29 20:18:31 -0300

    feat: criação da estrutura HTML e melhora de código no JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,23 @@
-alert("Seja bem-vindo a Vinharia Agnello");
-// Mensagem de recepção para usuário
+document.addEventListener("DOMContentLoaded", () => {
+// Espera a página terminar de carregar o HTML e só depois executa o JS   
 
-let nomeVinho = prompt("Insira o nome do vinho: ");
-let tipoVinho = prompt("Insira o tipo de vinho (Tinto, Branco, Rosé) ");
-let safraVinho = parseInt(prompt("Insira a safra deste vinho:"));
-let quantVinho = parseInt(prompt("Insira a quantidade desse vinho no estoque: "))
-// Criação de variáveis e valores atribuidos a elas
+    alert("Seja bem-vindo a Vinharia Agnello");
+    // Mensagem de recepção para usuário
 
+    let nomeVinho = prompt("Insira o nome do vinho: ");
+    let tipoVinho = prompt("Insira o tipo de vinho (Tinto, Branco, Rosé): ");
+    let safraVinho = parseInt(prompt("Insira a safra deste vinho:"));
+    let quantVinho = parseInt(prompt("Insira a quantidade desse vinho no estoque: "));
+    //// Criação de variáveis e valores atribuidos a elas
+
+    alert("Dados registrados! Cheque o console para mais informações");
+    // Alerta para usuário checar o console
+
+    console.log("Bem-vindo ao console!");
+    // Recpeção de usuário no console
+
+    console.log(
+        `Nome - ${nomeVinho} Tipo - ${tipoVinho} Safra - ${safraVinho} Quantidade - ${quantVinho} vinhos em estoque`
+        // Exebição de informações armazenadas nas variáveis sobre o vinho
+    );
+});
